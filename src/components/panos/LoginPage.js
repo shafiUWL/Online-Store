ebe1ad52c04c0fdc78f30cbe9d4b4d6a1dce3420
panostsl <82144588+panostsl@users.noreply.github.com>
2021-05-20 20:29:17 +0300

    Added the login and sign up components
    Added the sign up and log in components, along with their shared css file.

---
+++ src/components/panos/LoginPage.js
@@ -0,0 +1,30 @@
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap'
+import React from 'react'
+import './LoginRegister.css'
+
+function LoginPage() {
+    return (
+        <Form className='login-form'>
+            <h1 className='text-center'>Website.com</h1>
+            <h3 className='text-center'>Log in</h3>
+            <FormGroup>
+                <Label>Email</Label>
+                <Input type='email' placeholder='Email' />
+            </FormGroup>
+            <FormGroup>
+                <Label>Password</Label>
+                <Input type='password' placeholder='Password' />
+            </FormGroup>
+            <br/>
+            <div class="d-grid gap-2 col-12 mx-auto">
+                <button class="btn btn-success btn-lg" type="button">Log in</button>
+            </div>
+            <div className='text-center'>
+                <p className='p'>Don't have an account?  </p>
+                <a href='https://www.youtube.com/watch?v=Nj16V10FXJo'>Sign up</a>
+            </div>
+        </Form>
+    )
+}
+
+export default LoginPage
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import "bootstrap/dist/css/bootstrap.min.css";
+import LoginPage from './components/panos/LoginPage'
+import RegisterPage from './components/panos/RegisterPage'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
