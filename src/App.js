d37192401283fbd74c3465aeb23482172e161bc8
Mohammed Shafiul Haq Chowdhury <21409074@student.uwl.ac.uk>
2021-05-22 19:32:42 +0100

    website bugs fixed

--- src/App.js
+++ src/App.js
@@ -22,15 +22,14 @@ function App () {
 
  
 <div className="grid-container">
+
+
         <header className="row">
           <div>
             <a className="brand" href="/">
               The Grocery Store
             </a>
           </div>
-         
-        
-          
         </header>
         <main>
         <div className="container">
@@ -39,22 +38,24 @@ function App () {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/tempPage">Temp Page</Link></li>
           <li><Link to="/Compare">Compare</Link></li>
-         
           <li><Link to="/Payment">Payment</Link></li>
+          <li><Link to="/LoginPage">Log in/Register</Link></li>
+
         </ul>
       </nav>
     
-    </div>
+        </div>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
-        </main>
-        <Switch>
+          <Switch>
     <Route path="/Compare" component = {Compare}/>
     <Route path="/LoginPage" component = {LoginPage}/>
     <Route path="/RegisterPage" component = {RegisterPage}/>
     <Route path="/Payment" component = {Payment}/>
     </Switch>
-    <footer className="row center"> All Rights Reserved</footer>
+        </main>
+        
+    
       </div>
  
       
