b45e45d64950d0fb74bce8698df7859357294d40
Mohammed Shafiul Haq Chowdhury <21409074@student.uwl.ac.uk>
2021-05-22 19:25:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from 'react';
+import React from 'react';
 import './App.css';
 import Compare from './components/panos/Compare'
 
@@ -9,10 +9,7 @@ import LoginPage from './Pages/LoginPage'
 import RegisterPage from './Pages/RegisterPage'
 import Payment from './Pages/Payment'; 
 
-/** 
-<Route path="/Payment" component = {Payment}/>
- */
- 
+
 import HomeScreen from "./Pages/HomeScreen";
 import ProductScreen from "./Pages/ProductScreen";
  
@@ -57,34 +54,15 @@ function App () {
     <Route path="/RegisterPage" component = {RegisterPage}/>
     <Route path="/Payment" component = {Payment}/>
     </Switch>
-        
+    <footer className="row center"> All Rights Reserved</footer>
       </div>
  
-      <footer className="row center"> All Rights Reserved</footer>
+      
   </Router>
   
 )
 }
 
  
-/** Note this is being developed right now, and I will delete all my useless code
- 
- 
-function App(){
-  const[showItem, setShowItem] = useState(false)
-  return (
-    <div>
-      <h1>The Compare component</h1>
-        <p>Details and notes can be found within source code src/components/panos/compare</p>
-        <Compare/>
-      <h1>Payment Function</h1>
- 
-      {showItem ? <StripeContainer/> : <> <h3>£10.00</h3> <img src={orange} alt="Orange" /><button onClick={() => setShowItem(true)}>Purchase Item</button></>}
-      
- 
-    </div>
- 
-  )
-}
-*/
+
 export default App;
